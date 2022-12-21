1e5581e89efd9af977ff56f80648989540f5910c
WahoiJ <syuumai64@yahoo.co.jp>
2022-12-21 22:04:10 +0900

    index

--- adblock.js
+++ adblock.js
@@ -1,6 +1,7 @@
 window.onload = () => {
   const adspace = document.querySelector('#adspace');
   if( adspace.clientHeight === 0 ){
-    alert('テスト');
+    confirm(	'サイト管理人わくせいから拡張機能の広告ブロックを取り除くように提案があった。\nしかし世界各国では広告ブロックは合法との見方が強まっている');
   }
+  
 }
