799fcbbf748261d436d54c8d454cb0216fd0b313
WahoiJ <WahoiJ@users.noreply.github.com>
2025-09-14 16:46:22 +0000

    deploy: d2d90367ad693db5d52fc96ccd0ce8bc466fd2ba

--- adblock.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  const adspace = document.querySelector('#adspace');
-  if( adspace.clientHeight === 0 ){
-    confirm(	'サイト管理人わくせいから拡張機能の広告ブロックを取り除くように提案があった。\nしかし世界各国では広告ブロックは合法との見方が強まっている');
-  }
-  
-}
