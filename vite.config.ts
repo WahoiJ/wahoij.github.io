45d96fe0eb7bde80680c3bb21b10e6241c19b2bc
T <syuumai64@yahoo.co.jp>
2025-10-06 01:17:22 +0900

    Merge branch 'reBuild' of https://github.com/WahoiJ/wahoij.github.io into reBuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+<<<<<<< HEAD
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+=======
+export default defineConfig({
+  plugins: [
+    react(),
+    basicSsl() // SSL プラグインを追加
+  ],
+  server: {
+    https: true,
+    host: 'localhost',
+    port: 5173
+  }
+})
+>>>>>>> parent of 474e30d (本番環境と開発環境を分岐)
