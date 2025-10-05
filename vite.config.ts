7447f590f62085244f0110bcc049197d7d6c5aa8
T <syuumai64@yahoo.co.jp>
2025-10-06 01:03:24 +0900

    vs.config.tsをもとに戻す

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig(({ command }) => {
-  const isProduction = command === 'build'
-  
-  return {
-    plugins: [
-      react(),
-      basicSsl()
-    ],
-    base: isProduction ? '/myNewSite/' : '/', // 本番時のみリポジトリ名を使用
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      rollupOptions: {
-        output: {
-          manualChunks: undefined,
-        }
-      }
-    },
-    server: {
-      https: true,
-      host: 'localhost',
-      port: 5173
-    }
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 })
