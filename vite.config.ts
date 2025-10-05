474e30d5ee40811d2fa18ff9cf9f8d64cdc47468
T <syuumai64@yahoo.co.jp>
2025-10-06 00:51:15 +0900

    本番環境と開発環境を分岐

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    basicSsl() // SSL プラグインを追加
-  ],
-  server: {
-    https: true,
-    host: 'localhost',
-    port: 5173
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build'
+  
+  return {
+    plugins: [
+      react(),
+      basicSsl()
+    ],
+    base: isProduction ? '/myNewSite/' : '/', // 本番時のみリポジトリ名を使用
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        }
+      }
+    },
+    server: {
+      https: true,
+      host: 'localhost',
+      port: 5173
+    }
   }
 })
